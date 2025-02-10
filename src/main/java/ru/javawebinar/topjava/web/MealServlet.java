0e355e1fc1c8169c77d94240eb98d54da3935fb9
Vladislav Lee <arngrey@yandex.ru>
2025-02-11 00:07:23 +0300

    Add meals table

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,21 +1,33 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.repository.Repository;
+import ru.javawebinar.topjava.util.MealsUtil;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
     public static final Logger log = getLogger(MealServlet.class);
+    public static final Repository<Meal> mealRepository = new MealRepository();
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         log.debug("redirect to meals");
 
-        response.sendRedirect("meals.jsp");
+        List<MealTo> meals = MealsUtil.filteredByStreams(mealRepository.getAll(), LocalTime.MIN, LocalTime.MAX, MealsUtil.CALORIES_PER_DAY);
+        request.setAttribute("meals", meals);
+
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,4 +1,6 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" %>
+
 <html lang="ru">
 <head>
     <title>Meals</title>
@@ -7,5 +9,37 @@
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+<br>
+<table>
+    <tr>
+        <td style="text-align:center;">Date</td>
+        <td style="text-align:center;">Description</td>
+        <td style="text-align:center;">Calories</td>
+        <td style="text-align:center;"></td>
+        <td style="text-align:center;"></td>
+    </tr>
+    <c:forEach items="${meals}" var="meal">
+        <c:choose>
+            <c:when test="${!meal.excess}">
+                <tr style="color:green;border-top:1px solid black;">
+                    <td>${meal.formattedDateTime}</td>
+                    <td>${meal.description}</td>
+                    <td>${meal.calories}</td>
+                    <td>update</td>
+                    <td>delete</td>
+                </tr>
+            </c:when>
+            <c:otherwise>
+                <tr style="color:red;">
+                    <td>${meal.formattedDateTime}</td>
+                    <td>${meal.description}</td>
+                    <td>${meal.calories}</td>
+                    <td>update</td>
+                    <td>delete</td>
+                </tr>
+            </c:otherwise>
+        </c:choose>
+    </c:forEach>
+</table>
 </body>
 </html>
