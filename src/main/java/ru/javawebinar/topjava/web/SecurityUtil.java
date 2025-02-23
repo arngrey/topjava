c985127936328adcac97f588a9bf4e8b15230cab
Vladislav Lee <arngrey@yandex.ru>
2025-02-23 15:58:20 +0300

    Add userId to meal

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -4,8 +4,10 @@ import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
 
+    public static final int ADMIN_USER_ID = 1;
+
     public static int authUserId() {
-        return 1;
+        return ADMIN_USER_ID;
     }
 
     public static int authUserCaloriesPerDay() {
