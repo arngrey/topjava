65233a53e3977a588d43a5e64eadb07aefae0e6f
Vladislav Lee <arngrey@yandex.ru>
2025-02-23 17:02:24 +0300

    Make meal repository implementation thread safe

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -8,9 +8,12 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
 
 public class InMemoryMealRepository implements MealRepository {
+    private static final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
     private final Map<Integer, Meal> mealsMap = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
@@ -20,41 +23,64 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Meal save(int userId, Meal meal) {
-        if (meal.getUserId() != userId) {
-            return null;
+        lock.writeLock().lock();
+        try {
+            if (meal.getUserId() != userId) {
+                return null;
+            }
+            if (meal.isNew()) {
+                meal.setId(counter.incrementAndGet());
+                mealsMap.put(meal.getId(), meal);
+                return meal;
+            }
+            // handle case: update, but not present in storage
+            return mealsMap.computeIfPresent(
+                    meal.getId(),
+                    (id, oldMeal) -> oldMeal.getUserId() == userId ? meal : null
+            );
+        } finally {
+            lock.writeLock().unlock();
         }
-        if (meal.isNew()) {
-            meal.setId(counter.incrementAndGet());
-            mealsMap.put(meal.getId(), meal);
-            return meal;
-        }
-        // handle case: update, but not present in storage
-        return mealsMap.computeIfPresent(
-                meal.getId(),
-                (id, oldMeal) -> oldMeal.getUserId() == userId ? meal : null
-        );
     }
 
     @Override
     public boolean delete(int userId, int id) {
-        Meal meal = get(userId, id);
-        if (meal == null) {
-            return false;
+        lock.writeLock().lock();
+        try {
+            Meal meal = mealsMap.get(id);
+            if (meal.getUserId() != userId) {
+                return false;
+            }
+            return mealsMap.remove(id) != null;
+        } finally {
+            lock.writeLock().lock();
         }
-        return mealsMap.remove(id) != null;
     }
 
     @Override
     public Meal get(int userId, int id) {
-        Meal meal = mealsMap.get(id);
-        return meal.getUserId() == userId ? meal : null;
+        lock.readLock().lock();
+        try {
+            Meal meal = mealsMap.get(id);
+            if (meal.getUserId() != userId) {
+                return null;
+            }
+            return meal;
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public Collection<Meal> getAll(int userId) {
-        return mealsMap.values().stream()
-                .filter(meal -> meal.getUserId() == userId)
-                .collect(Collectors.toList());
+        lock.readLock().lock();
+        try {
+            return mealsMap.values().stream()
+                    .filter(meal -> meal.getUserId() == userId)
+                    .collect(Collectors.toList());
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
 
