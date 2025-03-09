7addf1bb2a38b48c0ba5ac40553e357f2e29fc27
Vladislav Lee <arngrey@yandex.ru>
2025-03-09 23:21:05 +0300

    Add init anp populate scripts for meals table

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_role;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -10,3 +11,9 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES
+    ('2025-01-01 10:23:54', 'Breakfast 1', 300, 100000),
+    ('2025-01-01 11:23:54', 'Breakfast 2', 300, 100000),
+    ('2025-01-01 12:23:54', 'Breakfast 3', 300, 100000);
