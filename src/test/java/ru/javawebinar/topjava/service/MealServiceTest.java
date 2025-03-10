1076edd1cf56e03c05df54b23d33c7e697032338
Vladislav Lee <arngrey@yandex.ru>
2025-03-11 00:05:11 +0300

    Split context into inmemory and jdbc

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -22,12 +22,11 @@ import static ru.javawebinar.topjava.UserTestData.GUEST_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ContextConfiguration({
-        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-app-jdbc.xml",
         "classpath:spring/spring-db.xml"
 })
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@Qualifier("InMemoryMealRepository")
 public class MealServiceTest {
 
     static {
